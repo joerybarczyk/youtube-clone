c0da6c7709514025e02023365cacceff51ce4d59
michael-prye <michael.prye@gmail.com>
2022-05-03 17:25:12 -0400

    Updated table for HomeVideoCard Component

--- frontend/src/components/HomeVideoCard/HomeVideoCard.jsx
+++ frontend/src/components/HomeVideoCard/HomeVideoCard.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 import { useNavigate, Link } from "react-router-dom";
 import { useState } from "react/cjs/react.production.min";
+
 
 const HomeVideoCard = (props) => {
   const navigate = useNavigate();
@@ -10,18 +12,32 @@ const HomeVideoCard = (props) => {
     navigate(`/watch?v=${props.video.videoId}`);
   };
   return (
-    <div onClick={handleClick}>
-      <img
-        class="card-img-top"
-        src={props.video.thumbnail.url}
-        alt="Thumbnail URL"
-      />
-      <div class="card-body">
-        <h5 class="card-title">{props.video.title}</h5>
-        <h6 class="card-text">{props.video.channelName}</h6>
-        <p>{props.video.date}</p>
-      </div>
-    </div>
+    <Card>
+      <Card.Img variant="top" src={props.video.thumbnail.url}/>
+      <Card.Title>{props.video.title}</Card.Title>
+      <Card.Text>{props.video.channelName}</Card.Text>
+      <Card.Text>{props.video.date}</Card.Text>
+
+    </Card>
+
+    
+
+
+
+
+
+    // <div onClick={handleClick}>
+    //   <img
+    //     class="card-img-top"
+    //     src={props.video.thumbnail.url}
+    //     alt="Thumbnail URL"
+    //   />
+    //   <div class="card-body">
+    //     <h5 class="card-title">{props.video.title}</h5>
+    //     <h6 class="card-text">{props.video.channelName}</h6>
+    //     <p>{props.video.date}</p>
+    //   </div>
+    // </div>
   );
 };
 
