2ec709fa68de10671112bc57c98c19a13a1862eb
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-05-03 17:03:50 -0400

    Add filtering for deleted videos in useRelatedVideo hook + Remove console.logs

--- frontend/src/hooks/useRelatedVideos.js
+++ frontend/src/hooks/useRelatedVideos.js
@@ -13,24 +13,27 @@ const useRelatedVideos = function (videoId) {
     let response = await axios.get(
       `https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${videoId}&type=video&key=${KEY}`
     );
-
-    console.log(response.data.items);
     const formattedResponse = formatRawResults(response.data.items);
     setRelatedVideos(formattedResponse);
   }
 
   function formatRawResults(rawResults) {
-    const formattedResults = rawResults.map((vidObj) => {
-      const formattedVidObj = {
-        videoId: vidObj.id.videoId,
-        channelTitle: vidObj.snippet.channelTitle,
-        description: vidObj.snippet.description,
-        publishTime: vidObj.snippet.publishTime,
-        thumbnail: vidObj.snippet.thumbnails.medium,
-        title: vidObj.snippet.title,
-      };
-      return formattedVidObj;
-    });
+    const formattedResults = rawResults
+      .filter((vidObj) => {
+        return vidObj.snippet !== undefined;
+      })
+      .map((vidObj) => {
+        const formattedVidObj = {
+          videoId: vidObj.id.videoId,
+          channelTitle: vidObj.snippet.channelTitle,
+          description: vidObj.snippet.description,
+          publishTime: vidObj.snippet.publishTime,
+          thumbnail: vidObj.snippet.thumbnails.medium,
+          title: vidObj.snippet.title,
+        };
+        return formattedVidObj;
+      });
+    console.log(formattedResults);
     return formattedResults;
   }
 
--- frontend/src/pages/HomePageYouTube/HomePageYouTube.js
+++ frontend/src/pages/HomePageYouTube/HomePageYouTube.js
@@ -2,33 +2,25 @@ import React from "react";
 import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 import useVideoSearchResults from "../../hooks/useVideoSearchResults";
-import HomeVideoCard from "../../components/HomeVideoCard/HomeVideoCard"
-
+import HomeVideoCard from "../../components/HomeVideoCard/HomeVideoCard";
 
 const HomePageYouTube = () => {
   const [searchResults] = useVideoSearchResults("kittens");
 
-  if (searchResults) {
-    console.log("results from useVideoSearchResults");
-    console.log(searchResults);
-  }
-
   return (
     <div>
       <p> Hello World!!!</p>
-      {searchResults != null &&
+      {searchResults != null && (
         <tbody>
-          {searchResults.map((video)=>{
-            return(
-              <td><HomeVideoCard video={video}  /></td>
-            )
-          }
-
-          )}
+          {searchResults.map((video) => {
+            return (
+              <td>
+                <HomeVideoCard video={video} />
+              </td>
+            );
+          })}
         </tbody>
-      }
-
-
+      )}
     </div>
   );
 };
